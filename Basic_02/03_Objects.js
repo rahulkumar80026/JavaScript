89ac2ddda2e6894a44d78aa1c69c79ce781ef577
Rahul kumar <92114290+rahulkumar80026@users.noreply.github.com>
2024-11-26 18:15:19 +0000

    object first

---
+++ Basic_02/03_Objects.js
@@ -0,0 +1,39 @@
+
+// singleton
+// Object.create
+
+// object literals
+
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Rahul",
+    "full name": "Rahul Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "Rahul@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "Rahul@chatgpt.com"
+// Object.freeze(JsUser)
+JsUser.email = "Rahul@microsoft.com"
+// console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting());
+console.log(JsUser.greetingTwo());
